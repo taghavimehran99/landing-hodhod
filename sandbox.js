afa39199376630989d1f5e526109f4eb1fc0d6fb
Mehran Taghavi <M.Taghavi@farabi.local>
2025-03-09 15:34:52 +0330

    fix mor item show in project  just open can not to close

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+const faq=document.getElementById('mor-faq')
+const btnMor=document.getElementById('btn-faq')
+
+
+btnMor.addEventListener("click",(e)=>{
+    e.preventDefault();
+    faq.className="mor-faq-show";
+})
+
+
+
+
+
+
+
+
+
+
+
